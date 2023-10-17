4ffa4515f8c318d243dea4fafc6222c732477340
lukejans <91230392+lukejans@users.noreply.github.com>
2023-10-16 20:16:13 -0400

    Update css

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const black_0 = `#15141b`;
 const black_1 = '#343a40';
 const black_2 = '#495057';
 const black_3 = '#adb5bd';
-const highlight = '#00FFD128';
+const highlight = '#3C00FF35';
 
 exports.decorateConfig = (config) => {
   return Object.assign({}, config, {
@@ -46,23 +46,32 @@ exports.decorateConfig = (config) => {
     },
     css: `
       ${config.css || ''}
+      .tabs_title {
+        display: flex;
+        justify-content: center;
+        gap: 4px;
+        color: ${purple_2};
+      }
       .tabs_title::before {
-        content: "ƪ(˘⌣˘)ʃ ";
-        color: ${purple_3};
+        content: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 48 48'><rect width='48' height='48' fill='white' fill-opacity='0.01' /><path d='M15.5 9L7 14V24V34L15.5 39L24 44L32.5001 39L41 34V24V14L32.5001 9L24 4L15.5 9Z' stroke='%236a74fc' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /><path d='M24 4L24 24' stroke='%236a74fc' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /><path d='M41 34L24 24' stroke='%236a74fc' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /><path d='M7 34L24 24' stroke='%236a74fc' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /><path d='M24 44V34' stroke='%236a74fc' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /><path d='M41 14L33 19' stroke='%236a74fc' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /><path d='M7 14L15 19' stroke='%236a74fc' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /></svg>");
+        margin-top: 3px;
+        display: block;
+        width: 12px;
+        height: 12px;
       }
       .tab_active {
         color: ${purple_2};
       }
       .tab_text::before {
         content: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 48 48'><rect width='48' height='48' fill='white' fill-opacity='0.01' /><path d='M15.5 9L7 14V24V34L15.5 39L24 44L32.5001 39L41 34V24V14L32.5001 9L24 4L15.5 9Z' stroke='%23dee2e6' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /><path d='M24 4L24 24' stroke='%23dee2e6' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /><path d='M41 34L24 24' stroke='%23dee2e6' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /><path d='M7 34L24 24' stroke='%23dee2e6' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /><path d='M24 44V34' stroke='%23dee2e6' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /><path d='M41 14L33 19' stroke='%23dee2e6' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /><path d='M7 14L15 19' stroke='%23dee2e6' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /></svg>");
-        margin-top: 2px;
+        margin-top: 3px;
         display: block;
         width: 12px;
         height: 12px;
       }
       .tab_textActive::before {
         content: url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 48 48'><rect width='48' height='48' fill='white' fill-opacity='0.01' /><path d='M15.5 9L7 14V24V34L15.5 39L24 44L32.5001 39L41 34V24V14L32.5001 9L24 4L15.5 9Z' stroke='%236a74fc' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /><path d='M24 4L24 24' stroke='%236a74fc' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /><path d='M41 34L24 24' stroke='%236a74fc' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /><path d='M7 34L24 24' stroke='%236a74fc' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /><path d='M24 44V34' stroke='%236a74fc' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /><path d='M41 14L33 19' stroke='%236a74fc' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /><path d='M7 14L15 19' stroke='%236a74fc' stroke-width='4' stroke-linecap='round' stroke-linejoin='round' /></svg>");
-        margin-top: 2px;
+        margin-top: 3px;
         display: block;
         width: 12px;
         height: 12px;
